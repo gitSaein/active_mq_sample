a94a90cbd2f11df8757849b5eb82dde8d757b169
saein-lee <saeinlee@mytalk.co.kr>
2020-10-21 14:27:45 +0900

    Initial commit

---
+++ src/main/java/com/example/mqtt_message/MqttMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mqtt_message/MqttMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
